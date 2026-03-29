45f43b61272eb6666c3d86bd76b9b650eb66e54b
Pri Patel <priyankp295@gmail.com>
2026-03-28 23:52:20 -0400

    Update CORS
    Updates cors to allow frontend to reach backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ import resumesRoutes from './routes/ResumeRoutes.js';
 
 const app = express();
 
+
+// CORS configuration
+const allowedOrigins = [
+  "https://professional-portfolio-wheat.vercel.app/",
+  "http://localhost:3000"
+];
+
 app.use(cors());
 app.use(express.json());
 
@@ -14,6 +21,33 @@ connectDB();
 // Routes
 app.use('/api/resumes', resumesRoutes);
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+
 // Start server
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/resumes", resumesRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
